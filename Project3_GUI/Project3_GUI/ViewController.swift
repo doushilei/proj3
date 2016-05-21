97b942bb7d36838ad557d9f3786fdc8ddf454a5d
BlackTea <wubh@shanghaitech.edu.cn>
2016-05-21 15:19:24 +0800

    3

---
+++ Project3_GUI/Project3_GUI/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Project3_GUI
+//
+//  Created by Blacktea on 16/5/21.
+//  Copyright © 2016年 Dou_Shi_Lei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+  
+
+}
+
---
+++ Project_GUI/Project_GUI/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  Project_GUI
+//
+//  Created by Blacktea on 16/5/21.
+//  Copyright © 2016年 Dou_Shi_Lei. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+  @IBOutlet weak var dataLabel: UILabel!
+  var dataObject: String = ""
+
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+  override func viewWillAppear(animated: Bool) {
+    super.viewWillAppear(animated)
+    self.dataLabel!.text = dataObject
+  }
+
+
+}
+
